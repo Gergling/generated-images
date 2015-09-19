106468477ccae7f1928f39c70ea415eef4784931
gergling <gregmichaeldavies@gmail.com>
2015-09-19 16:18:21 +0100

    Indentation fix.

--- src/grunt/paths.js
+++ src/grunt/paths.js
@@ -1,28 +1,34 @@
 module.exports = {
-  vendor: [
-      // Must be included first
-      'src/public/vendor/jquery/jquery.js',
-      'src/public/vendor/angular/angular.js',
-      'src/public/vendor/lodash/lodash.compat.js',
+    js: {
+        vendor: [
+            // Must be included first
+            'src/public/vendor/jquery/jquery.js',
+            'src/public/vendor/angular/angular.js',
+            'src/public/vendor/lodash/lodash.compat.js',
 
-      // Everything else
-      'src/public/vendor/*/*.js',
+            // Everything else
+            'src/public/vendor/*/*.js',
 
-      // Exclusions from standard vendor libraries
-      '!src/public/vendor/angular-mocks/angular-mocks.js',
-      'src/public/vendor/bootstrap/bootstrap.js'
-  ],
+            // Exclusions from standard vendor libraries
+            '!src/public/vendor/angular-mocks/angular-mocks.js',
+            'src/public/vendor/bootstrap/bootstrap.js'
+        ],
 
-  scripts: [
-      'src/public_html/module/**/module.js',
-      'src/public_html/module/**/*.js'
-  ],
-  client: [
-      'src/public/modules/**/module.js',
-      'src/public/modules/**/*.js'
-  ],
-  module: [ 'src/module/**/*.js' ],
+        scripts: [
+            'src/public/module/**/module.js',
+            'src/public/module/**/*.js'
+        ],
+        client: [
+            'src/public/modules/**/module.js',
+            'src/public/modules/**/*.js'
+        ],
+        module: [ 'src/module/**/*.js' ],
 
-  helpers: [ 'src/public/vendor/angular-mocks/*.js' ],
-  spec: grunt.file.expand('spec/client/**/*.js')
+        helpers: [ 'src/public/vendor/angular-mocks/*.js' ],
+        spec: grunt.file.expand('spec/client/**/*.js')
+    },
+    css: {
+        vendor: [ 'src/public/vendor/**/*.css' ],
+        styles: [ 'src/public/modules/**/*.css' ]
+    }
 };

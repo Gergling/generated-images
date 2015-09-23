19a83abaa7fdca87f7f6fcc014f86f941e5b4675
Greg <gregmichaeldavies@gmail.com>
2015-09-23 18:26:17 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,9 @@
 module.exports = function (grunt) {
     'use strict';
 
-    var paths = {
-            vendor: [
-                // Must be included first
-                'src/public/vendor/jquery/jquery.js',
-                'src/public/vendor/angular/angular.js',
-                'src/public/vendor/lodash/lodash.compat.js',
+    var gruntMod = function (mod) {return require('./src/grunt/' + mod);};
 
-                // Everything else
-                'src/public/vendor/*/*.js',
-
-                // Exclusions from standard vendor libraries
-                '!src/public/vendor/angular-mocks/angular-mocks.js',
-                'src/public/vendor/bootstrap/bootstrap.js'
-            ],
-
-            scripts: [
-                'src/public_html/module/**/module.js',
-                'src/public_html/module/**/*.js'
-            ],
-            client: [
-                'src/public/modules/**/module.js',
-                'src/public/modules/**/*.js'
-            ],
-            module: [ 'src/module/**/*.js' ],
-
-            helpers: [ 'src/public/vendor/angular-mocks/*.js' ],
-            spec: grunt.file.expand('spec/client/**/*.js')
-        },
-
-        css = {
-            vendor: [ 'src/public/vendor/**/*.css' ],
-            styles: [ 'src/public/modules/**/*.css' ]
-        },
+    var paths = gruntMod('paths'),
 
         banner = '/* Liftr */\n'
             + '/* <%= grunt.template.today("yyyy-mm-dd") %> */\n';

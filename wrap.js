2fb43fa197872a6d111f1982e93bf6faeedab7c2
gergling <gregmichaeldavies@gmail.com>
2015-10-03 00:33:56 +0100

    Wrapper script modified to run grunt as a node module.

--- wrap.js
+++ wrap.js
@@ -1,12 +1,25 @@
-var command = process.argv.slice(2).join(' ').replace(/\//g,'\\\\');
+//var command = process.argv.slice(2).join(' ').replace(/\//g,'\\\\');
+var command = process.argv[2];
 var isWin = /^win/.test(process.platform);
+var grunt = require('grunt');
+var args = process.argv.slice(3).join(' ').replace(/\//g,'\\\\');
 
-console.log('Wrap: Executing', command);
+console.log('Wrap: Executing', command, args);
 
-require('child_process').exec(command, function(error, stdout, stderr) {
+switch(command) {
+case 'grunt':
+    var grunt = require('grunt');
+    require('./Gruntfile')(grunt);
+    grunt.tasks(args);
+    break;
+case 'bower':
+    break;
+}
+
+/*require('child_process').exec(command, function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
     if (error !== null) {
         console.log('exec error: ' + error);
     }
-});
+});*/
